81832b36fdc521ff98297bc8fbb57cfa11832e79
Lorena Sucerquia <cristi.mayo@hotmail.com>
2026-07-08 11:58:42 -0500

    Primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-d241d6ec-5529-4cb9-98e3-28fe0053d3ad.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
